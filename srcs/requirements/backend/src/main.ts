96842e30f258c746638c09ac62f97d40b3b08421
speranos <aoueldma@student.1337.ma>
2023-11-05 03:20:29 +0100

    loading

--- srcs/requirements/backend/src/main.ts
+++ srcs/requirements/backend/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaClient } from '@prisma/client';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+
+function  swagger_set(app){
+  const config = new DocumentBuilder()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  swagger_set(app);
   await app.listen(3000);
 }
+
+
 bootstrap();
