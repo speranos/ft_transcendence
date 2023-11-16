ba3978628b4e88dea70dd54b4e9d49f3736ca008
speranos <aoueldma@student.1337.ma>
2023-11-16 13:24:52 +0100

    duplication_done

--- srcs/requirements/backend/src/auth/auth.service.ts
+++ srcs/requirements/backend/src/auth/auth.service.ts
@@ -1,18 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { Authdto } from 'src/dto';
 import { PrismaMdService } from 'src/prisma-md/prisma-md.service';
-// import { validate } from 'class-validator';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
-  constructor(private data: PrismaMdService){}
+  constructor(private db: PrismaMdService){}
   signin(): string {
     return 'signin';
   }
   async signup(dto: Authdto) {
+    try{
     const hash = await argon.hash(dto.password);
+    const user = await this.db.user.create({
+      data: {
+        username: dto.username,
+        email : dto.email,
+        Password: hash,
+      },
+    });
     console.log(hash);
-    return 'signup';
+    return user;
+  }
+  catch(error){
+    if(error instanceof PrismaClientKnownRequestError)
+      if(error.code === 'P2002'){
+        throw new ForbiddenException('User already exists!');
+      }
+  }
   }
 }
--- srcs/requirements/backend/src/dto/auth.dto.ts
+++ srcs/requirements/backend/src/dto/auth.dto.ts
@@ -3,6 +3,10 @@ import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class Authdto {
     @IsNotEmpty()
+    @IsString()
+    username: string;
+    
+    @IsNotEmpty()
     @IsEmail()
     email: string;
 
